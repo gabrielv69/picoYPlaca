3f212326dad4475b47fe7c66e9921ce670df218f
Gabriel Vivas <gabrielv-69@hotmail.com>
2021-04-07 02:28:20 -0500

    Pequeño cambio al Ejecutable

--- src/main/java/picoYPlaca/Ejecutable.java
+++ src/main/java/picoYPlaca/Ejecutable.java
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat;
 
 public class Ejecutable {
     public static void main (String[] args){
+        System.out.println("******************  Pico y Placa Predictor  ******************\n Ingreso de Datos");
         PicoYPlaca sistema = new PicoYPlaca();
         String resultado = sistema.circula(sistema.ingresarplate(),sistema.ingresarDate(),sistema.ingresarTime()) ? "\n Puede circular" : "\n No puede circular";
         System.out.println(resultado);
